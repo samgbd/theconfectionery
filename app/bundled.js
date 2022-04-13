9bf949e4a63c86647f18d9c05b740c195df3ae9e
sdevantha <donaldson.sgb@gmail.com>
2022-04-13 22:34:16 +0100

    testing ssh key works

--- app/bundled.js
+++ app/bundled.js
@@ -7,7 +7,6 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./app/assets/scripts/App.js":
@@ -16,17 +15,18 @@
   \***********************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../styles/styles.css */ \"./app/assets/styles/styles.css\");\n/* harmony import */ var _styles_scss_main_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../styles/scss/main.scss */ \"./app/assets/styles/scss/main.scss\");\n/* harmony import */ var _modules_MemoryJogger__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/MemoryJogger */ \"./app/assets/scripts/modules/MemoryJogger.js\");\n/* harmony import */ var _modules_NavMenu__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/NavMenu */ \"./app/assets/scripts/modules/NavMenu.js\");\n\r\n\r\n//* This is an example of how to import, and then call, your module files *\r\n\r\n\r\n\r\nnew _modules_MemoryJogger__WEBPACK_IMPORTED_MODULE_2__[\"default\"]();\r\nnew _modules_NavMenu__WEBPACK_IMPORTED_MODULE_3__[\"default\"]();\r\n//* Make sure you erase me once I've served my purpose *\r\n\r\nconsole.log(\"hey\")\r\n\r\nif (false) {}\n\n//# sourceURL=webpack://theconfectionery/./app/assets/scripts/App.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../styles/styles.css */ \"./app/assets/styles/styles.css\");\n/* harmony import */ var _styles_scss_main_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../styles/scss/main.scss */ \"./app/assets/styles/scss/main.scss\");\n/* harmony import */ var _modules_NavMenu__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/NavMenu */ \"./app/assets/scripts/modules/NavMenu.js\");\n/* harmony import */ var _modules_Form__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/Form */ \"./app/assets/scripts/modules/Form.js\");\n/* harmony import */ var _modules_Form__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_modules_Form__WEBPACK_IMPORTED_MODULE_3__);\n\r\n\r\n\r\n\r\n\r\nnew _modules_NavMenu__WEBPACK_IMPORTED_MODULE_2__[\"default\"]();\r\nnew (_modules_Form__WEBPACK_IMPORTED_MODULE_3___default())();\r\n\r\nconsole.log(\"hey\")\r\n\r\nif (false) {}\r\n\n\n//# sourceURL=webpack://theconfectionery/./app/assets/scripts/App.js?");
 
 /***/ }),
 
-/***/ "./app/assets/scripts/modules/MemoryJogger.js":
-/*!****************************************************!*\
-  !*** ./app/assets/scripts/modules/MemoryJogger.js ***!
-  \****************************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ "./app/assets/scripts/modules/Form.js":
+/*!********************************************!*\
+  !*** ./app/assets/scripts/modules/Form.js ***!
+  \********************************************/
+/***/ (() => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n//* This is an example of how to set up your javascript module files *\r\n//* Make sure you delete me once I've served my purpose *\r\nclass MemoryJogger {\r\n  constructor () {\r\n    this.hamburger = document.getElementById('hamburger');\r\n  }\r\n\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MemoryJogger);\n\n//# sourceURL=webpack://theconfectionery/./app/assets/scripts/modules/MemoryJogger.js?");
+eval("class Form {\r\n  constructor () {\r\n    this.hamburger = document.getElementById('hamburger');\r\n  }\r\n\r\n}\n\n//# sourceURL=webpack://theconfectionery/./app/assets/scripts/modules/Form.js?");
 
 /***/ }),
 
@@ -36,6 +36,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \***********************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nclass NavMenu {\r\n  constructor () {\r\n    this.hamburger = document.getElementById('hamburger');\r\n    this.navMenu = document.getElementById('navMenu');\r\n    this.navList = document.getElementById('navList');\r\n\r\n    this.testing()\r\n    this.events()\r\n  }\r\n\r\n  testing () {\r\n    console.log(\"hi\")\r\n    console.log(this.hamburger)\r\n  }\r\n\r\n  events(){\r\n    this.hamburger.addEventListener('click', () => this.toggleMenu());\r\n  }\r\n\r\n  toggleMenu () {\r\n    console.log(this.hamburger)\r\n    let navHeight = this.getHeight();\r\n    if (this.checkIfOpen()) {\r\n      this.hamburger.classList.remove('is-active')\r\n      this.navMenu.classList.remove('is-active')\r\n      this.navMenu.style.height = '0px';\r\n      console.log('close')\r\n    } else {\r\n      this.hamburger.classList.add('is-active')\r\n      this.navMenu.classList.add('is-active')\r\n      this.navMenu.style.height = `${navHeight}px`;\r\n      console.log('open')\r\n    }\r\n  }\r\n\r\n  checkIfOpen () {\r\n    return this.hamburger.classList.contains('is-active');\r\n  }\r\n\r\n  getHeight () {\r\n    return this.navList.offsetHeight;\r\n  }\r\n\r\n\r\n\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (NavMenu);\n\n//# sourceURL=webpack://theconfectionery/./app/assets/scripts/modules/NavMenu.js?");
 
 /***/ }),
@@ -46,6 +47,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \**************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://theconfectionery/./app/assets/styles/styles.css?");
 
 /***/ }),
@@ -56,6 +58,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
   \******************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://theconfectionery/./app/assets/styles/scss/main.scss?");
 
 /***/ })
@@ -87,6 +90,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
